be7aa97c7545811aea83a9f76da01ce720d0ae67
tgfisher <tuckergfisher@gmail.com>
2019-06-16 13:30:59 -0700

    Updates

---
+++ precache-manifest.227799564391117418cb9654d78ede47.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "66342b75aacb2224ddd6",
+    "url": "/static/css/main.2fc733fa.chunk.css"
+  },
+  {
+    "revision": "66342b75aacb2224ddd6",
+    "url": "/static/js/main.66342b75.chunk.js"
+  },
+  {
+    "revision": "5dbbf8b8f917765689bb",
+    "url": "/static/css/1.25f2b0d6.chunk.css"
+  },
+  {
+    "revision": "5dbbf8b8f917765689bb",
+    "url": "/static/js/1.5dbbf8b8.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c1f13ad5dedbf6f8c2409ed69ff90343",
+    "url": "/static/media/DSCF9079.c1f13ad5.jpg"
+  },
+  {
+    "revision": "1064032a35e5a164f7f36a46e981ee63",
+    "url": "/static/media/Datura_RIP.1064032a.jpg"
+  },
+  {
+    "revision": "88afb2a235df42974b6cc96ce1e9733a",
+    "url": "/index.html"
+  }
+];
