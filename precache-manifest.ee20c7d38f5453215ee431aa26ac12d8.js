7c1bc9bb33cd46fbb1a7b87f38d058702d700523
tgfisher <tuckergfisher@gmail.com>
2019-07-11 13:17:27 -0700

    Updates

---
+++ precache-manifest.ee20c7d38f5453215ee431aa26ac12d8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "614c70004f72fef0580d7663f4fdf1b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9becb70f2ef40e396afa",
+    "url": "/static/css/2.0f7a873c.chunk.css"
+  },
+  {
+    "revision": "29fc81bb95eefcc90888",
+    "url": "/static/css/main.ed6484f9.chunk.css"
+  },
+  {
+    "revision": "9becb70f2ef40e396afa",
+    "url": "/static/js/2.582718ee.chunk.js"
+  },
+  {
+    "revision": "29fc81bb95eefcc90888",
+    "url": "/static/js/main.24b90252.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c1f13ad5dedbf6f8c2409ed69ff90343",
+    "url": "/static/media/DSCF9079.c1f13ad5.jpg"
+  },
+  {
+    "revision": "1064032a35e5a164f7f36a46e981ee63",
+    "url": "/static/media/Datura_RIP.1064032a.jpg"
+  }
+]);
