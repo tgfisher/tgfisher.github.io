2c20f47583ab0e4e834377804f4dfa3d9fdd0e52
tgf <tuckergfisher@gmail.com>
2022-07-21 10:50:00 -0700

    Updates

---
+++ precache-manifest.9fbb3a5292d81eb564db59704bb5c0ab.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c383833a643c16d52543b3d4dd86f4cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9dcda68d8fee96dd2acb",
+    "url": "/static/css/2.e15a0812.chunk.css"
+  },
+  {
+    "revision": "4d685506a198045f5858",
+    "url": "/static/css/main.7a686be5.chunk.css"
+  },
+  {
+    "revision": "9dcda68d8fee96dd2acb",
+    "url": "/static/js/2.9608be68.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.9608be68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d685506a198045f5858",
+    "url": "/static/js/main.575fab5f.chunk.js"
+  },
+  {
+    "revision": "6ad345ac74f200aa74df",
+    "url": "/static/js/runtime-main.3e5e9dc4.js"
+  },
+  {
+    "revision": "1064032a35e5a164f7f36a46e981ee63",
+    "url": "/static/media/Datura_RIP.1064032a.jpg"
+  },
+  {
+    "revision": "7d154d93ab6ba7cae8db02f5c2d3a425",
+    "url": "/static/media/iceberg.7d154d93.jpg"
+  }
+]);
